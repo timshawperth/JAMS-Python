d29564b2c537169949566d1a6cc9a0ed0e5de69b
Tim Shaw <timshawperth@gmail.com>
2020-09-07 17:06:14 +0800

    Allows creation of job as well as looking up a job by instantiating a Job Object. Forgot that JAMSreturns a 201 code upon successful addition of a job.

--- jamswrapper/auth.py
+++ jamswrapper/auth.py
@@ -1,13 +1,15 @@
 import requests
 
 class Auth:
+    _h = {'content-type':'application/json'}
+
     def __init__(self, host, username, password):
         self._host = host
         self._auth_request = {'username':username, 'password':password}
 
     def login(self):
         target = 'http://{}/jams/api/authentication/login'.format(self._host)
-        r = requests.post(target, data = self._auth_request)
+        r = requests.post(target,  data=self._auth_request)
         if r.status_code != requests.codes.ok:
             r.raise_for_status()
         self._auth_response = r.json()
@@ -20,5 +22,5 @@ class Auth:
         return self._auth_response['access_token']
 
     def header(self):
-        return {'Authorization': 'bearer {}'.format(self.access_token())}
+        return {'Authorization': 'bearer {}'.format(self.access_token()), 'content-type':'application/json'}
 
--- jamswrapper/job.py
+++ jamswrapper/job.py
@@ -1,22 +1,42 @@
 import requests
 
 class Job:
+    _auth = None
+    _target = None
     _job = None
+    _newjob = {'jobName':'', 'parentFolderName':'', 'description':'', 'methodName':''}
 
-    def __init__(self, auth, jobname=None):
+    def __init__(self, auth, jobname,  parent_folder_name=None, method_name=None, desc=''):
+        if jobname == None:
+            raise 'Must supply a job name'
+
         self._auth = auth
         self._target = 'http://{}/jams/api/job'.format(auth.host())
-        if jobname != None:
+
+        if parent_folder_name == None and method == None:
             self.load(jobname)
+        else:
+            self._newjob['jobName'] = jobname
+            self._newjob['parentFolderName'] = parent_folder_name
+            self._newjob['methodName'] = method_name
+            self._newjob['description'] = desc
+            self._create()
+
+    def _create(self):
+        r = requests.post(self._target, headers=self._auth.header(), json=self._newjob)
+        # A 201 is a successful result of creatnig a job
+        if r.status_code != 201:
+            r.raise_for_status()
+        else:
+            self.load(self._newjob['jobName'])
 
     def load(self, jobname):
         q_param = {'name': jobname}
-        r = requests.get(self._target, headers = self._auth.header(), params=q_param)
+        r = requests.get(self._target, headers=self._auth.header(), params=q_param)
         if r.status_code != requests.codes.ok:
             r.raise_for_status()
         else:
             self._job = r.json()
-#            return self._job
 
     def get(self, attr):
         return self._job[attr]
@@ -29,7 +49,7 @@ class Job:
     def update(self):
         if self._job == None:
             raise Exception('No job available to update')
-        r = requests.put(self._target, headers = self._auth.header(), json=self._job)
+        r = requests.put(self._target, headers=self._auth.header(), json=self._job)
         if r.status_code != requests.codes.ok:
             r.raise_for_status()
 
